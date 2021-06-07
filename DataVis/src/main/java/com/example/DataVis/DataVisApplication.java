a496ac2644b16ab02733e110a7ac88baf3e2124d
Abhishek Mane <whitewolf@gatrio.pro>
2021-06-06 21:09:39 -0700

    Springboot project from spring.start.io

---
+++ DataVis/src/main/java/com/example/DataVis/DataVisApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataVis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataVisApplication.class, args);
+	}
+
+}
